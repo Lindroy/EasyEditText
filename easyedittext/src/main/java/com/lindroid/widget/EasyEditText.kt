17fa8f1359d132fbc819d9b98322763c6ba03e67
Lindroy <linyulong2014@163.com>
2019-03-30 16:00:20 +0800

    创建项目，并编写部分代码。

---
+++ easyedittext/src/main/java/com/lindroid/widget/EasyEditText.kt
@@ -0,0 +1,156 @@
+package com.lindroid.widget
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.AppCompatEditText
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+
+/**
+ * @author Lin
+ * @date 2019/3/30
+ * @function 自定义EditText
+ * @Description
+ */
+class EasyEditText : AppCompatEditText {
+    private val icClearId = R.drawable.ic_clear
+    private val icShowPwd = R.drawable.ic_pwd_visible
+    private val icHidePwd = R.drawable.ic_pwd_invisible
+
+    /**
+     * 超过最大输入字符数时的提示文字
+     */
+    var maxToastText = ""
+
+    /**
+     * 最大输入字符数，-1表示不做限制
+     */
+    var maxInputLength = -1
+        set(value) {
+            field = value
+            if (field != -1) {
+                setTextWatcher()
+            }
+        }
+
+    /**
+     * 最小输入字符数，小于0表示不做限制
+     */
+    var minInputLength = 0
+        set(value) {
+            field = value
+            if (field > 0) {
+                setTextWatcher()
+            }
+        }
+
+    /**
+     * 是否显示清空按钮
+     */
+    var isShowClearButton = false
+        set(value) {
+            field = value
+            if (field) {
+                setTextWatcher()
+            } else {
+//                clearRightDrawable()
+            }
+        }
+
+    /**
+     * 是否显示设置密码可见的按钮，前提是不显示删除按钮
+     */
+    var isShowPwdButton = false
+        set(value) {
+            field = value
+            if (!isShowClearButton && field) {
+//                initPwdBtn()
+            } else {
+//                clearRightDrawable()
+            }
+        }
+
+    /**
+     * 一键清空按钮
+     */
+    private val ivClear: Drawable? by lazy {
+        ContextCompat.getDrawable(context, icClearId)
+    }
+
+    private var textWatcher: TextWatcher? = null
+
+    private var textListener: ((s: CharSequence) -> Unit)? = null
+
+    constructor(context: Context?) : this(context, null)
+    //默认style为R.attr.editTextStyle才能获取焦点
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, android.R.attr.editTextStyle)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        val typedArray = context?.obtainStyledAttributes(attrs, R.styleable.EasyEditText, defStyleAttr, 0)
+        typedArray?.let {
+            //            toastMaxStr = it.getString(R.styleable.CustomEditText_toastMaxStr) ?: ""
+//            maxInputLength = it.getInt(R.styleable.CustomEditText_maxInputLength, maxInputLength)
+//            minInputLength = it.getInt(R.styleable.CustomEditText_minInputLength, minInputLength)
+//            isShowClear = it.getBoolean(R.styleable.CustomEditText_showClearBtn, isShowClear)
+//            isShowPwdBtn = it.getBoolean(R.styleable.CustomEditText_showClearBtn, isShowPwdBtn)
+            it.recycle()
+        }
+
+//        init()
+
+    }
+
+    private fun setTextWatcher() {
+        if (textWatcher != null) {
+            textWatcher
+            removeTextChangedListener(textWatcher)
+        }
+
+        textWatcher = object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                textListener?.invoke(s ?: "")
+            }
+        }
+        addTextChangedListener(textWatcher)
+
+    }
+
+    /**
+     * 绘制删除图片
+     */
+    private fun setClearDrawable() {
+        when (length() < 1) {
+            true -> setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+            false -> setCompoundDrawablesWithIntrinsicBounds(null, null, ivClear, null)
+        }
+    }
+
+    /**
+     * 绘制设置密码是否可以见的图标
+     */
+    private fun setPwdDrawable(isPwdVisible: Boolean) {
+        val ivPwd = when (isPwdVisible) {
+            true -> ContextCompat.getDrawable(context, icShowPwd)
+            false -> ContextCompat.getDrawable(context, icHidePwd)
+        }
+        setCompoundDrawablesWithIntrinsicBounds(null, null, ivPwd, null)
+    }
+
+
+    fun setOnTextChangeListener(listener: (content: CharSequence) -> Unit) {
+        textListener = listener
+    }
+
+
+}
